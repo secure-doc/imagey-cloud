ed03dc8c4a8ae566ce233ba403f17bcc2a9da0b4
arne <arne@apache.org>
2026-05-30 13:50:43 +0200

    fix: ImageActivity handling

--- imagey-web/src/activity/Activity.ts
+++ imagey-web/src/activity/Activity.ts
@@ -3,17 +3,20 @@ import Document from "../document/Document";
 export type Activity = InvitationActivity | ImageActivity | UploadActivity;
 
 export interface InvitationActivity {
+  id: string;
   type: ActivityType.INVITATION;
   userName: string;
   message?: string;
 }
 
 export interface ImageActivity {
+  id: string;
   type: ActivityType.IMAGE;
   image: Document;
 }
 
 export interface UploadActivity {
+  id: string;
   type: ActivityType.UPLOAD;
 }
 
